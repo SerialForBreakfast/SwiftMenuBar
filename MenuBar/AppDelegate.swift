9a77652a1a099cf39d2312d31729bea35bd7e734
Joe <joe@showblender.com>
2017-07-12 14:15:56 -0700

    Basic MenuBar app
    Displays resolution as MenuBarItem

---
+++ MenuBar/AppDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  AppDelegate.swift
+//  MenuBar
+//
+//  Created by Joseph McCraw on 7/12/17.
+//  Copyright © 2017 Joseph McCraw. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        
+        statusItem.button?.title = "Fetching..."
+        
+        if let screenInfo: [String : Any] = (NSScreen.main()?.deviceDescription)! {
+            for info in screenInfo {
+                print(info)
+                print(screenInfo["NSDeviceSize"]!)
+                if let sizeInfo = screenInfo["NSDeviceSize"] as! CGSize!{
+                    print(sizeInfo.width)
+                    print(sizeInfo.height)
+                    let roundedWidth: Int = Int(sizeInfo.width)
+                    let roundedHeight: Int = Int(sizeInfo.height)
+                    let cleanText: String = "\(roundedWidth) x \(roundedHeight)"
+                    statusItem.button?.title = cleanText
+                }
+            }
+        }
+        
+        statusItem.menu = NSMenu()
+        addConfigurationMenuItem()
+        
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func addConfigurationMenuItem() {
+        let seperator = NSMenuItem(title: "Settings", action: #selector(showSettings), keyEquivalent: "")
+        statusItem.menu?.addItem(seperator)
+    }
+    
+    func showSettings(_ sender: NSMenuItem) {
+        //Launch Settings
+        
+    }
+    
+}
+
---
+++ MenuBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBar
+//
+//  Created by Joseph McCraw on 7/12/17.
+//  Copyright © 2017 Joseph McCraw. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
